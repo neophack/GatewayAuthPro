6ed88a63a62fa20d8218a849093c8e2be48b377a
LambdaExpression <LambdaExpression@139.com>
2021-10-29 16:50:58 +0800

    Test

--- build.sh
+++ build.sh
@@ -9,9 +9,11 @@ go-bindata -o=../../src/bindata/bindata.go  -pkg=bindata   -fs ./...
 
 cd ../..
 
+mkdir -p build
+
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build ./src/main.go
-mv main gatewayAuth_linux_arm_$version
+mv main ./build/gatewayAuth_linux_arm_$version
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./src/main.go
-mv main gatewayAuth_linux_amd64_$version
+mv main ./build/gatewayAuth_linux_amd64_$version
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build ./src/main.go
-mv main gatewayAuth_darwin_amd64_$version
+mv main ./build/gatewayAuth_darwin_amd64_$version
