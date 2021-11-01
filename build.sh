d96c3f8409f57b170ab9b9c1153b1ace67be5dd2
LambdaExpression <LambdaExpression@139.com>
2021-11-01 15:44:46 +0800

    Test Github Actions

--- build.sh
+++ build.sh
@@ -11,35 +11,15 @@ cd ../..
 
 mkdir -p build
 
-go mod download
-go mod vendor
-go mod verify
-
-
-
-echo "\n\n"
-
-ls /home/runner/work/GatewayAuth/GatewayAuth/src/config
-
-echo "\n\n"
-
-ls /home/runner/work/GatewayAuth/GatewayAuth/src
-
-echo "\n\n"
-
-echo "\n\n======================== ls =========================\n\n"
-
-ls
-pwd
-
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build ./src/main.go
-
-echo "\n\n======================== build =========================\n\n"
-
-ls
-
 mv main ./build/gatewayAuth_linux_arm_$version
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./src/main.go
 mv main ./build/gatewayAuth_linux_amd64_$version
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build ./src/main.go
 mv main ./build/gatewayAuth_darwin_amd64_$version
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build ./src/main.go
+mv main.exe ./build/gatewayAuth_windows_amd64_$version.exe
+
+echo "\n\n======================== build =========================\n\n"
+
+ls ./build/
