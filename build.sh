d021399d60ab0829fc7aea78a51d6451f01085b4
LambdaExpression <LambdaExpression@139.com>
2021-11-01 14:29:31 +0800

    Test Github Actions

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ cd ../..
 
 mkdir -p build
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build ./src/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build /home/runner/work/GatewayAuth/GatewayAuth/src/main.go
 mv main ./build/gatewayAuth_linux_arm_$version
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./src/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build /home/runner/work/GatewayAuth/GatewayAuth/src/main.go
 mv main ./build/gatewayAuth_linux_amd64_$version
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build ./src/main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build /home/runner/work/GatewayAuth/GatewayAuth/src/main.go
 mv main ./build/gatewayAuth_darwin_amd64_$version
